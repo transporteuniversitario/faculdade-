82f4be04612baeb8721764e7ef6aa2f882a64dd3
transporteuniversitario <transporteescolarbelmonte2022@gmail.com>
2025-04-12 13:04:50 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+CONFIG = {
+    "logo_prefeitura_padrao": "static/logos/logo_prefeitura.png",
+    "logo_secretaria_padrao": "static/logos/logo_secretaria.png",
+    "assinatura_padrao": "static/assinaturas/assinatura_padrao.png",
+    "nome_secretario_padrao": "Maria Silva",
+    "cabecalho_padrao": "SECRETARIA MUNICIPAL DE EDUCAÇÃO DE BELMONTE",
+    "validade_padrao": "12/2025"
+}
